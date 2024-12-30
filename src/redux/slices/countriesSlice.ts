1675dd918ec3bcc4e7347a90393668285b15fee5
m1erla <116915007+m1erla@users.noreply.github.com>
2024-12-31 00:16:19 +0300

    revised CORS settings

--- src/redux/slices/countriesSlice.ts
+++ src/redux/slices/countriesSlice.ts
@@ -16,40 +16,59 @@ const initialState: CountriesState = {
 export const fetchCountries = createAsyncThunk(
   "countries/fetchCountries",
   async () => {
-    const response = await fetch("/v3.1/all");
-    if (!response.ok) {
-      throw new Error("Failed to fetch countries");
-    }
-    const data = await response.json();
-    return data.map((country: any) => {
-      const currencyKey = country.currencies ? Object.keys(country.currencies)[0] : null;
-      const currencyInfo = currencyKey ? country.currencies[currencyKey] : null;
-
-      return {
-        name: country.name.common,
-        capital: country.capital?.[0] || "N/A",
-        population: country.population,
-        continent: country.region,
-        languages: Object.values(country.languages || {}),
-        flag: country.flags.svg,
-        area: country.area,
-        currencies: Object.values(country.currencies || {}).map((curr: any) => curr.name),
-        timezones: country.timezones,
-        currency: currencyInfo ? {
-          name: currencyInfo.name,
-          code: currencyKey,
-          symbol: currencyInfo.symbol
-        } : {
-          name: "N/A",
-          code: "N/A",
-          symbol: "N/A"
+    try {
+      const response = await fetch("https://restcountries.com/v3.1/all", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
         },
-        coordinates: {
-          latitude: country.latlng?.[0] || 0,
-          longitude: country.latlng?.[1] || 0
-        }
-      };
-    });
+        mode: 'cors',
+        cache: 'no-cache',
+      });
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const text = await response.text();
+      try {
+        const data = JSON.parse(text);
+        return data.map((country: any) => {
+          const currencyKey = country.currencies ? Object.keys(country.currencies)[0] : null;
+          const currencyInfo = currencyKey ? country.currencies[currencyKey] : null;
+
+          return {
+            name: country.name.common,
+            capital: country.capital?.[0] || "N/A",
+            population: country.population,
+            continent: country.region,
+            languages: Object.values(country.languages || {}),
+            flag: country.flags.svg,
+            area: country.area,
+            currencies: Object.values(country.currencies || {}).map((curr: any) => curr.name),
+            timezones: country.timezones,
+            currency: currencyInfo ? {
+              name: currencyInfo.name,
+              code: currencyKey,
+              symbol: currencyInfo.symbol
+            } : {
+              name: "N/A",
+              code: "N/A",
+              symbol: "N/A"
+            },
+            coordinates: {
+              latitude: country.latlng?.[0] || 0,
+              longitude: country.latlng?.[1] || 0
+            }
+          };
+        });
+      } catch (error) {
+        throw error;
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 );
 
